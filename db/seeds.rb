80f6b2099adad0079001c7508ee171afb3baeada
ioilmio <illuminato.salvatore@gmail.com>
2020-09-01 17:18:14 +0200

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+30.times do |i|
+    User.create!(name: "user#{i}",
+                password: 'password',
+                email: "user#{i}@mail.com")
+    end
