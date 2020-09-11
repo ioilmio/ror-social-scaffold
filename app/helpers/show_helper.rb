3e369ae9143cf77a3ed3c8b57e19db704e828673
ioilmio <illuminato.salvatore@gmail.com>
2020-09-11 16:30:27 +0200

    fix multiple requests from user show page

--- app/helpers/show_helper.rb
+++ app/helpers/show_helper.rb
@@ -4,7 +4,7 @@ module ShowHelper
       link_to '<button type="button">Unfriend</button>'.html_safe,
               friendship_destroy_path(user),
               method: :delete, data: { confirm: 'Are you sure you want to Unfriend' }
-    elsif current_user != user
+    elsif current_user != user && !current_user.pending_friends.include?(user)
       link_to '<button type="button">Send Request</button>'.html_safe,
               friendships_path(user_id: current_user.id, friend_id: user.id),
               method: :post
